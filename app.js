d54fbb1c51362f32c10187740cc0b0ffe15ed163
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-11-05 19:18:17 -0300

    ok

---
+++ app.js
@@ -0,0 +1,13 @@
+function find () {
+
+    let capitalbase = document.getElementById('input1').value;
+    let tipo = document.getElementById('input2').value;
+
+    if (capitalbase == 100000 && tipo == 'essencial'){
+
+        console.log('40,69');
+    }
+
+
+}
+
