2d7153569e7430573f6e2530fc70e5c68731fd59
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-11-06 16:29:51 -0300

    Add

--- app.js
+++ app.js
@@ -7,6 +7,42 @@ function find () {
 
         console.log('40,69');
     }
+    if (capitalbase == 200000 && tipo == 'essencial'){
+
+        console.log('40,69');
+    }
+    if (capitalbase == 300000 && tipo == 'essencial'){
+
+        console.log('40,69');
+    }
+    if (capitalbase == 400000 && tipo == 'essencial'){
+
+        console.log('40,69');
+    }
+    if (capitalbase == 500000 && tipo == 'essencial'){
+
+        console.log('40,69');
+    }
+    if (capitalbase == 600000 && tipo == 'essencial'){
+
+        console.log('40,69');
+    }
+    if (capitalbase == 700000 && tipo == 'essencial'){
+
+        console.log('40,69');
+    }
+    if (capitalbase == 800000 && tipo == 'essencial'){
+
+        console.log('40,69');
+    }
+    if (capitalbase == 900000 && tipo == 'essencial'){
+
+        console.log('40,69');
+    }
+    if (capitalbase == 1000000 && tipo == 'essencial'){
+
+        console.log('40,69');
+    }
 
 
 }
