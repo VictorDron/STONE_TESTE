dbb35a60e634058c1cbb6af16922afe75e86941a
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-11-06 19:49:45 -0300

    OK

--- app.js
+++ app.js
@@ -21,38 +21,47 @@ function find () {
                         if (capitalbase == 200000){
     
                             console.log('57,18');
+                            price.innerText='R$'+' '+'57,18';
                         }
                         if (capitalbase == 300000){
     
                             console.log('83,64');
+                            price.innerText='R$'+' '+'83,64';
                         }
                         if (capitalbase == 400000){
     
                             console.log('110,10');
+                            price.innerText='R$'+' '+'110,10';
                         }
                         if (capitalbase == 500000){
     
                             console.log('136,57');
+                            price.innerText='R$'+' '+'136,57';
                         }
                         if (capitalbase == 600000){
     
                             console.log('155,40');
+                            price.innerText='R$'+' '+'155,40';
                         }
                         if (capitalbase == 700000){
     
                             console.log('174,24');
+                            price.innerText='R$'+' '+'174,24';
                         }
                         if (capitalbase == 800000){
     
                             console.log('193,06');
+                            price.innerText='R$'+' '+'193,06';
                         }
                         if (capitalbase == 900000){
     
                             console.log('211,90');
+                            price.innerText='R$'+' '+'211,90';
                         }
                         if (capitalbase == 1000000){
     
                             console.log('230,73');
+                            price.innerText='R$'+' '+'230,73';
                         }
                         
                         break;
@@ -62,42 +71,52 @@ function find () {
                         if (capitalbase == 100000){
     
                             console.log('54,51');
+                            price.innerText='R$'+' '+'54,51';
                         }
                         if (capitalbase == 200000){
     
                             console.log('77,21');
+                            price.innerText='R$'+' '+'77,21';
                         }
                         if (capitalbase == 300000){
     
                             console.log('113,67');
+                            price.innerText='R$'+' '+'113,67';
                         }
                         if (capitalbase == 400000){
     
                             console.log('150,14');
+                            price.innerText='R$'+' '+'150,14';
                         }
                         if (capitalbase == 500000){
     
                             console.log('186,60');
+                            price.innerText='R$'+' '+'186,60';
                         }
                         if (capitalbase == 600000){
     
                             console.log('215,46');
+                            price.innerText='R$'+' '+'215,46';
                         }
                         if (capitalbase == 700000){
     
                             console.log('244,30');
+                            price.innerText='R$'+' '+'244,30';
                         }
                         if (capitalbase == 800000){
     
                             console.log('273,13');
+                            price.innerText='R$'+' '+'273,13';
                         }
                         if (capitalbase == 900000){
     
                             console.log('301,98');
+                            price.innerText='R$'+' '+'301,98';
                         }
                         if (capitalbase == 1000000){
     
                             console.log('330,80');
+                            price.innerText='R$'+' '+'330,80';
                         }
             
                         break;
