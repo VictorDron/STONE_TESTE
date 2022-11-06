5f767b28f58f44abf5a14f84a0d6a48deeb9a098
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-11-06 17:19:19 -0300

    ok

--- app.js
+++ app.js
@@ -2,47 +2,210 @@ function find () {
 
     let capitalbase = document.getElementById('input1').value;
     let tipo = document.getElementById('input2').value;
+    let negocio = document.getElementById('input3').value;
 
-    if (capitalbase == 100000 && tipo == 'essencial'){
+        switch (negocio) {
+            case 'comercio':
+                
+                switch (tipo) {
+                    case 'essencial':
+    
+                        if (capitalbase == 100000){
+    
+                            console.log('40,69');
+                        }
+                        if (capitalbase == 200000){
+    
+                            console.log('57,18');
+                        }
+                        if (capitalbase == 300000){
+    
+                            console.log('83,64');
+                        }
+                        if (capitalbase == 400000){
+    
+                            console.log('110,10');
+                        }
+                        if (capitalbase == 500000){
+    
+                            console.log('136,57');
+                        }
+                        if (capitalbase == 600000){
+    
+                            console.log('155,40');
+                        }
+                        if (capitalbase == 700000){
+    
+                            console.log('174,24');
+                        }
+                        if (capitalbase == 800000){
+    
+                            console.log('193,06');
+                        }
+                        if (capitalbase == 900000){
+    
+                            console.log('211,90');
+                        }
+                        if (capitalbase == 1000000){
+    
+                            console.log('230,73');
+                        }
+                        
+                        break;
+    
+                    case 'completo':
+    
+                        if (capitalbase == 100000){
+    
+                            console.log('54,51');
+                        }
+                        if (capitalbase == 200000){
+    
+                            console.log('77,21');
+                        }
+                        if (capitalbase == 300000){
+    
+                            console.log('113,67');
+                        }
+                        if (capitalbase == 400000){
+    
+                            console.log('150,14');
+                        }
+                        if (capitalbase == 500000){
+    
+                            console.log('186,60');
+                        }
+                        if (capitalbase == 600000){
+    
+                            console.log('215,46');
+                        }
+                        if (capitalbase == 700000){
+    
+                            console.log('244,30');
+                        }
+                        if (capitalbase == 800000){
+    
+                            console.log('273,13');
+                        }
+                        if (capitalbase == 900000){
+    
+                            console.log('301,98');
+                        }
+                        if (capitalbase == 1000000){
+    
+                            console.log('330,80');
+                        }
+            
+                        break;
+                
+                    default:
+                        break;
+                }
+                break;
 
-        console.log('40,69');
-    }
-    if (capitalbase == 200000 && tipo == 'essencial'){
-
-        console.log('40,69');
-    }
-    if (capitalbase == 300000 && tipo == 'essencial'){
-
-        console.log('40,69');
-    }
-    if (capitalbase == 400000 && tipo == 'essencial'){
-
-        console.log('40,69');
-    }
-    if (capitalbase == 500000 && tipo == 'essencial'){
-
-        console.log('40,69');
-    }
-    if (capitalbase == 600000 && tipo == 'essencial'){
-
-        console.log('40,69');
-    }
-    if (capitalbase == 700000 && tipo == 'essencial'){
-
-        console.log('40,69');
-    }
-    if (capitalbase == 800000 && tipo == 'essencial'){
-
-        console.log('40,69');
-    }
-    if (capitalbase == 900000 && tipo == 'essencial'){
-
-        console.log('40,69');
-    }
-    if (capitalbase == 1000000 && tipo == 'essencial'){
-
-        console.log('40,69');
-    }
+            case 'serviços':
+                
+                switch (tipo) {
+                    case 'essencial':
+    
+                        if (capitalbase == 100000){
+    
+                            console.log('27,65');
+                        }
+                        if (capitalbase == 200000){
+    
+                            console.log('35,84');
+                        }
+                        if (capitalbase == 300000){
+    
+                            console.log('51,86');
+                        }
+                        if (capitalbase == 400000){
+    
+                            console.log('67,86');
+                        }
+                        if (capitalbase == 500000){
+    
+                            console.log('83,89');
+                        }
+                        if (capitalbase == 600000){
+    
+                            console.log('94,32');
+                        }
+                        if (capitalbase == 700000){
+    
+                            console.log('104,75');
+                        }
+                        if (capitalbase == 800000){
+    
+                            console.log('115,18');
+                        }
+                        if (capitalbase == 900000){
+    
+                            console.log('125,61');
+                        }
+                        if (capitalbase == 1000000){
+    
+                            console.log('136,03');
+                        }
+                        
+                        break;
+    
+                    case 'completo':
+    
+                        if (capitalbase == 100000){
+    
+                            console.log('39,86');
+                        }
+                        if (capitalbase == 200000){
+    
+                            console.log('53,76');
+                        }
+                        if (capitalbase == 300000){
+    
+                            console.log('78,73');
+                        }
+                        if (capitalbase == 400000){
+    
+                            console.log('103,67');
+                        }
+                        if (capitalbase == 500000){
+    
+                            console.log('128,64');
+                        }
+                        if (capitalbase == 600000){
+    
+                            console.log('148,05');
+                        }
+                        if (capitalbase == 700000){
+    
+                            console.log('167,43');
+                        }
+                        if (capitalbase == 800000){
+    
+                            console.log('186,83');
+                        }
+                        if (capitalbase == 900000){
+    
+                            console.log('206,20');
+                        }
+                        if (capitalbase == 1000000){
+    
+                            console.log('225,59');
+                        }
+            
+                        break;
+                
+                    default:
+                        break;
+                }
+                break;
+                
+            default:
+                break;
+        }
+            
+                    
 
 
 }
